027a1709cddf79e966cba70c01e2958b9f1c28e9
Arnaud Belcour <arnaud.belcour@irisa.fr>
2018-06-04 10:56:11 +0200

    Numerous issues fix (made by Jacques Nicolas).

--- pathmodel/wrapping_PathModel.py
+++ pathmodel/wrapping_PathModel.py
@@ -3,15 +3,17 @@
 import sys  
 import clyngor
 
+print('~~~~~Creation of Reaction~~~~~')
 next(clyngor.solve(['data.lp', 'asp/ReactionCreation.lp']), None)
 
+print('~~~~~Inference of reactions and metabolites~~~~~')
 test_result = clyngor.solve(['data_result.lp', 'asp/PathModel.lp'])
 
 result = clyngor.solve(['data_result.lp', 'asp/PathModel.lp'])
 
+print('~~~~~Creating result file~~~~~')
 resultfile = open("result.lp", "w")
 optimization_scores = []
-
 # Parse result of classification
 # Check the best optimization.
 # In clyngor with optimization answer are set with two datas: first one is the model the second is the optimization.
@@ -26,7 +28,7 @@ for answer in test_result.with_optimization:
 if optimization_scores:
 	best_score = max(optimization_scores)
 
-for answer in result.sorted.parse_args.atoms_as_string.int_not_parsed.with_optimization:
+for answer in sorted(result.parse_args.atoms_as_string.int_not_parsed.with_optimization):
 	try:
 		if answer[1][1] == best_score:
 			for atom in answer[0]:
