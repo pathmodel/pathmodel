7c2f5798c8859600ae51926e82a52ec559709aa1
Arnaud Belcour <arnaud.belcour@irisa.fr>
2019-02-04 17:25:22 +0100

    Fix test location.

--- test/test_pathmodel_wrapper.py
+++ test/test_pathmodel_wrapper.py
@@ -3,7 +3,7 @@
 import pathmodel
 
 def test_pathmodel():
-	resultfile_content = pathmodel.pathmodel_analysis('test_data/pathmodel_test_data.lp', picture_name=None, output_file=None)
+	resultfile_content = pathmodel.pathmodel_analysis('test/pathmodel_test_data.lp', picture_name=None, output_file=None)
 	print(resultfile_content)
 	result_expected = """newreaction(saturation,"molecule_3","molecule_4").\nnewreaction(saturation,"molecule_5","Prediction_78379269").\nreaction(saturation,"molecule_1","molecule_2").\nreaction(saturation,"molecule_3","molecule_4").\nreaction(saturation,"molecule_5","Prediction_78379269")."""
 	print(result_expected)
