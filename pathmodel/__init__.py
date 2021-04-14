0727528adc8aab9ca64dafe1e323256f15265fed
Arnaud Belcour <arnaud.belcour@inria.fr>
2021-04-14 21:02:57 +0200

    Move to 0.2.1. And try to fix issue with install.

--- pathmodel/__init__.py
+++ pathmodel/__init__.py
@@ -1,3 +1,3 @@
 from pathmodel.pathmodel_wrapper import check_folder, mz_computation, reaction_creation, pathmodel_inference, pathmodel_analysis
 
-__version__='0.2.0'
+__version__='0.2.1'
--- setup.py
+++ setup.py
@@ -2,18 +2,12 @@ import os
 from distutils.util import convert_path
 from setuptools import setup, find_packages
 
-init_data = {}
-init_pathname = convert_path('pathmodel/__init__.py')
-with open(init_pathname) as init_file:
-    exec(init_file.read(), init_data)
-
 setup_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(setup_directory, 'README.rst'), encoding='utf-8') as readme_file:
     long_description = readme_file.read()
 
 setup(
     name='pathmodel',
-    version=init_data['__version__'],
     url='https://github.com/pathmodel/pathmodel',
     description='Ab initio pathway inference',
     long_description=long_description,
